f259a90d62b428b329ddbb6b7af596597b4ce5bf
andreecampos <orlandoandree1998@gmail.com>
2021-09-22 14:24:56 +0200

    testing

---
+++ index.js
@@ -0,0 +1,39 @@
+/*class Animal {
+    constructor(name, legs){
+        this.name = name
+        this.legs = legs
+    }
+    speech (){
+        return `${this.name} makes a noise`
+    }
+    numberOfLegs  ()  {
+        return `This animal has ${this.legs} legs`
+    }
+}
+
+const dog = new Animal("Dog", 4)
+console.log(dog.speech())
+/*
+const cat = new Animal("Cat", 4)
+console.log(cat.speech())
+console.log(cat.numberOfLegs())
+console.log(cat.name)*/
+
+/*class Voter {
+    constructor(age) {
+        this.age = age
+    }
+    canVote(){
+        if (this.age >= 18){
+            return true} else { return false }
+       //canVote = () => this.age >= 18
+}
+
+
+const voter1 = new Voter (23)
+console.log(voter1.canVote())
+
+
+const voter2 = new Voter (23)
+console.log(voter2.canVote())*/
+  
